1f7dc292579aed619d26ce585b4a6e1dce99d2c5
Farzonakhon Izatullaeva <farzonasland@gmail.com>
2025-10-28 12:16:18 -0400

    made it more efficient

--- rock_paper_scisors_game.py
+++ rock_paper_scisors_game.py
@@ -1,10 +1,36 @@
 import random
 options = ['rock', 'paper', 'scissors']
+'''
+rock -> rock = tie
+     -> paper = 2
+     -> scissors = 1
+    [0, 2, 1]
+
+paper -> rock = 1
+      -> paper = tie
+      -> scissors = 2
+    [1, 0, 2]
+
+scissors -> rock = 2
+         -> paper = 1
+         -> scissors = tie
+    [2, 1, 0]
+'''
+rock_conditions = [0, 2, 1]
+paper_conditions = [1, 0, 2]
+scissors_conditions = [2, 1, 0]
+
+index_decision = {
+    0: "Tie",
+    1: 'User Wins',
+    2: "Computer Wins"
+}
+
 
 while True:
     random_number = random.randint(1, 3)
-    computer_choice = options[random_number-1]
-
+    computer_choice_index = random_number-1
+    computer_choice = options[computer_choice_index]
     print("Rock, paper, scissors? (r/p/s): ")
     user_choice = input().lower().strip()
 
@@ -12,68 +38,90 @@ while True:
         print("Invalid choice!")
 
     else:
+
         if (user_choice == 'r'):
-            if (computer_choice == 'rock'):
+            decision = rock_conditions[computer_choice_index]
+            if (decision == 0):
                 print(f"Computer chose {computer_choice}")
-                print("Tie!")
+                value = index_decision[decision]
+                print(value)
+
                 print("Continue? (y/n): ")
                 continue_game = input().lower()
                 if (continue_game == 'n'):
                     break
-            elif (computer_choice == 'paper'):
+            elif (decision == 1):
                 print(f"Computer chose {computer_choice}")
-                print("Computer Wins!")
+                value = index_decision[decision]
+                print(value)
+
                 print("Continue? (y/n): ")
                 continue_game = input().lower()
                 if (continue_game == 'n'):
                     break
-            elif (computer_choice == 'scissors'):
+            elif (decision == 2):
                 print(f"Computer chose {computer_choice}")
-                print("You Win!")
+                value = index_decision[decision]
+                print(value)
+
                 print("Continue? (y/n): ")
                 continue_game = input().lower()
                 if (continue_game == 'n'):
                     break
         elif (user_choice == 'p'):
-            if (computer_choice == 'paper'):
+            decision = paper_conditions[computer_choice_index]
+            if (decision == 0):
                 print(f"Computer chose {computer_choice}")
-                print("Tie!")
+                value = index_decision[decision]
+                print(value)
+
                 print("Continue? (y/n): ")
                 continue_game = input().lower()
                 if (continue_game == 'n'):
                     break
-            elif (computer_choice == 'scissors'):
+            elif (decision == 1):
                 print(f"Computer chose {computer_choice}")
-                print("Computer Wins!")
+                value = index_decision[decision]
+                print(value)
+
                 print("Continue? (y/n): ")
                 continue_game = input().lower()
                 if (continue_game == 'n'):
                     break
-            elif (computer_choice == 'rock'):
+            elif (decision == 2):
                 print(f"Computer chose {computer_choice}")
-                print("You Win!")
+                value = index_decision[decision]
+                print(value)
+
                 print("Continue? (y/n): ")
                 continue_game = input().lower()
                 if (continue_game == 'n'):
                     break
         elif (user_choice == 's'):
-            if (computer_choice == 'scissors'):
+            decision = scissors_conditions[computer_choice_index]
+            if (decision == 0):
                 print(f"Computer chose {computer_choice}")
-                print("Tie!")
+                value = index_decision[decision]
+                print(value)
+
                 print("Continue? (y/n): ")
                 continue_game = input().lower()
                 if (continue_game == 'n'):
                     break
-            elif (computer_choice == 'rock'):
+            elif (decision == 1):
                 print(f"Computer chose {computer_choice}")
-                print("Computer Wins!")
+                value = index_decision[decision]
+                print(value)
+
                 print("Continue? (y/n): ")
                 continue_game = input().lower()
                 if (continue_game == 'n'):
                     break
-            elif (computer_choice == 'paper'):
+            elif (decision == 2):
                 print(f"Computer chose {computer_choice}")
-                print("You Win!")
+                value = index_decision[decision]
+                print(value)
+
                 print("Continue? (y/n): ")
                 continue_game = input().lower()
                 if (continue_game == 'n'):
