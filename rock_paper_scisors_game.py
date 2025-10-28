8024f6433e572383a83ca05be1280b9ccba3c521
Farzonakhon Izatullaeva <farzonasland@gmail.com>
2025-10-28 12:51:45 -0400

    made changes and removed repetition

--- rock_paper_scisors_game.py
+++ rock_paper_scisors_game.py
@@ -26,6 +26,11 @@ index_decision = {
     2: "Computer Wins"
 }
 
+user_choice_to_conditions = {
+    'r': rock_conditions,
+    'p': paper_conditions,
+    's': scissors_conditions
+}
 
 while True:
     random_number = random.randint(1, 3)
@@ -36,93 +41,13 @@ while True:
 
     if (user_choice not in ('r', 'p', 's')):
         print("Invalid choice!")
+        continue
 
-    else:
+    decision = user_choice_to_conditions[user_choice][computer_choice_index]
+    print(f"Computer chose {computer_choice}")
+    print(index_decision[decision])
 
-        if (user_choice == 'r'):
-            decision = rock_conditions[computer_choice_index]
-            if (decision == 0):
-                print(f"Computer chose {computer_choice}")
-                value = index_decision[decision]
-                print(value)
-
-                print("Continue? (y/n): ")
-                continue_game = input().lower()
-                if (continue_game == 'n'):
-                    break
-            elif (decision == 1):
-                print(f"Computer chose {computer_choice}")
-                value = index_decision[decision]
-                print(value)
-
-                print("Continue? (y/n): ")
-                continue_game = input().lower()
-                if (continue_game == 'n'):
-                    break
-            elif (decision == 2):
-                print(f"Computer chose {computer_choice}")
-                value = index_decision[decision]
-                print(value)
-
-                print("Continue? (y/n): ")
-                continue_game = input().lower()
-                if (continue_game == 'n'):
-                    break
-        elif (user_choice == 'p'):
-            decision = paper_conditions[computer_choice_index]
-            if (decision == 0):
-                print(f"Computer chose {computer_choice}")
-                value = index_decision[decision]
-                print(value)
-
-                print("Continue? (y/n): ")
-                continue_game = input().lower()
-                if (continue_game == 'n'):
-                    break
-            elif (decision == 1):
-                print(f"Computer chose {computer_choice}")
-                value = index_decision[decision]
-                print(value)
-
-                print("Continue? (y/n): ")
-                continue_game = input().lower()
-                if (continue_game == 'n'):
-                    break
-            elif (decision == 2):
-                print(f"Computer chose {computer_choice}")
-                value = index_decision[decision]
-                print(value)
-
-                print("Continue? (y/n): ")
-                continue_game = input().lower()
-                if (continue_game == 'n'):
-                    break
-        elif (user_choice == 's'):
-            decision = scissors_conditions[computer_choice_index]
-            if (decision == 0):
-                print(f"Computer chose {computer_choice}")
-                value = index_decision[decision]
-                print(value)
-
-                print("Continue? (y/n): ")
-                continue_game = input().lower()
-                if (continue_game == 'n'):
-                    break
-            elif (decision == 1):
-                print(f"Computer chose {computer_choice}")
-                value = index_decision[decision]
-                print(value)
-
-                print("Continue? (y/n): ")
-                continue_game = input().lower()
-                if (continue_game == 'n'):
-                    break
-            elif (decision == 2):
-                print(f"Computer chose {computer_choice}")
-                value = index_decision[decision]
-                print(value)
-
-                print("Continue? (y/n): ")
-                continue_game = input().lower()
-                if (continue_game == 'n'):
-                    break
+    print("Continue? (y/n): ")
+    continue_game = input().lower()
+    if (continue_game == 'n'):
+        break
